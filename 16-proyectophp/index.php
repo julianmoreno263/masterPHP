2f3bf650cdc34357940d53568f274c811a0146e6
julianmoreno263 <julianmoreno263@gmail.com>
2024-03-07 21:25:29 -0500

    creando los estilos css del blog

--- 16-proyectophp/index.php
+++ 16-proyectophp/index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog de Videojuegos PHP</title>
+
+    <link rel="stylesheet" href="./recursos-blog/assets/css/style.css">
 </head>
 <body>
 
