ad35c68fbbb863301e91252a74b867d03dc382e0
julianmoreno263 <julianmoreno263@gmail.com>
2024-04-17 15:27:23 -0500

    Herencia en POO en php

---
+++ 17-POO/3-herencia/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+require_once 'clases.php';
+
+//creo mis objetos
+$persona =new Persona();
+var_dump($persona);
+
+$informatico=new Informatico();
+var_dump($informatico);
+
+$tecnico=new TecnicoRedes();
+var_dump($tecnico);
+
+
+
+
+
+
+?>
