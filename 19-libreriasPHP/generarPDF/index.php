ea10d59603e6a5532ff93bb9e60513f46404cb9d
Julián Moreno <julianmoreno263@gmail.com>
2024-05-23 17:20:29 -0500

    trabajando con composer para instalar librerias en php

---
+++ 19-libreriasPHP/generarPDF/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+//en este modulo veremos como instalar librerias con composer y como usarlas en nuestros proyectos php.Esta libreria convierte archivos html a pdf.
+
+//el composer lo instalamos en el pc globalmente y las librerias las instalamos en la carpeta donde vayamos a usarlas,entonces debemos pararnos en la carpeta correspondiente en consola.
+
+
+require '../vendor/autoload.php';
+
+//asi llamamos a la libreria
+use Spipu\Html2Pdf\Html2Pdf;
+
+//creamos un codigo html
+// $html="<h1 style='background:red'>Hola mundo desde una libreria php para hacer pdfs</h1>";
+// $html.="<p style='background:orange; color:white'>Master en php por Julian Moreno</p>";
+
+//tambien podemos tomar un codigo html externo para convertirlo a pdf, para eso hacemos:
+
+ob_start();//captura el html que tengamos
+require_once 'pdfParaGenerar.php';//cargamos el archivo donde esta el codigo html
+$html=ob_get_clean();//guardamos el html en la variable
+
+//creamos un objeto de la clase Html2Pdf
+$html2Pdf=new Html2Pdf();
+
+//uso un metodo de esa clase para cargar mi html y con output() lo paso a pdf poniendole un nombre de archivo con extension .pdf, asi cuando yo abra esta pagina en el navegador me sale un pdf automaticamente.Si modifico el html y actualizo la pagina me sale el pdf actualizado.
+$html2Pdf->writeHTML($html);
+$html2Pdf->output('pdfGenerado.pdf');
+
+
+?>
---
+++ 19-libreriasPHP/generarPDF/pdfParaGenerar.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generar pdf</title>
+
+    <style>
+
+        body{
+            font-family:Helvetica;
+        }
+
+        h1{
+            background:red;
+            font-size:20px;
+        }
+
+        p{
+            font-size:20px;
+        }
+
+        ul{
+            background:green;
+            color:white;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Hola mundo desde una libreria php para hacer pdfs</h1>
+    <p>Master en php por Julian Moreno</p>
+
+    <h3>Cursos de Programación</h3>
+    <ul>
+        <li>Curso Php</li>
+        <li>Curso Javascript</li>
+        <li>Curso Python</li>
+    </ul>
+</body>
+</html>
