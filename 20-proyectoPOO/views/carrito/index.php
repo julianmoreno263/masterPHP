7868b0e1a0eea164d94699d6d47ce22bb8e183e5
unknown <julianmoreno263@gmail.com>
2024-08-07 14:12:03 -0500

    finalizacion del proyecto tienda-master

--- 20-proyectoPOO/views/carrito/index.php
+++ 20-proyectoPOO/views/carrito/index.php
@@ -24,7 +24,13 @@
 
             <td><a href="<?=base_url?>producto/ver&id=<?=$producto->id?>"><?=$producto->nombre?></a></td>
             <td><?=$producto->precio?></td>
-            <td><?=$elemento['unidades']?></td>
+            <td>
+                <?=$elemento['unidades']?>
+                <div class="updown-unidades">
+                    <a href="<?=base_url?>carrito/up&index=<?=$indice?>" class="button">+</a>
+                    <a href="<?=base_url?>carrito/down&index=<?=$indice?>" class="button">-</a>
+                </div>
+            </td>
             <td>
                 <a href="<?=base_url?>carrito/delete&index=<?=$indice?>" class="button button-carrito button-red">Eliminar</a>
             </td>
